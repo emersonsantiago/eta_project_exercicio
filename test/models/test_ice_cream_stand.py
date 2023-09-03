a816b45dca9b78819c108d6cef95086901e8459e
Emerson Santiago <emersoncostasantiago@gmail.comm>
2023-09-03 01:22:28 -0300

    exercicio finalizado

--- test/models/test_ice_cream_stand.py
+++ test/models/test_ice_cream_stand.py
@@ -1,10 +1,49 @@
-class TestIceCreamStand:
+import unittest
+from io import StringIO
 
+from src.models.ice_cream_stand import IceCreamStand
+
+
+class TestIceCreamStand(unittest.TestCase):
     def test_flavors_available(self):
-        assert False
+        # Setup
+        restaurant_name = "Tio Beto Sorvetes"
+        cuisine_type = "Sorvetes Naturais"
+        flavors_list = ["Milho verde", "Chocolate", "Morango ao leite"]
+        ice_cream_stand = IceCreamStand(restaurant_name, cuisine_type, flavors_list)
+        expected_result = "No momento temos os seguintes sabores de sorvete disponíveis: Milho verde, Chocolate, Morango ao leite"
+
+        # Chamada
+        result = ice_cream_stand.flavors_available()
+
+
+        # Validação
+        assert expected_result == result
 
     def test_find_flavor(self):
-        assert False
+        # Setup
+        restaurant_name = "Tio Beto Sorvetes"
+        cuisine_type = "Sorvetes Naturais"
+        flavors_list = ["Milho verde", "Chocolate", "Morango ao leite"]
+        ice_cream_stand = IceCreamStand(restaurant_name, cuisine_type, flavors_list)
+        expected_result = "Temos no momento Chocolate"
+
+        # Chamada
+        result = ice_cream_stand.find_flavor("Chocolate")
+
+        # Validação
+        assert expected_result == result
 
     def test_add_flavor(self):
-        assert False
+        # Setup
+        restaurant_name = "Tio Beto Sorvetes"
+        cuisine_type = "Sorvetes Naturais"
+        flavors_list = ["Milho verde", "Chocolate", "Morango ao leite"]
+        ice_cream_stand = IceCreamStand(restaurant_name, cuisine_type, flavors_list)
+        expected_result = f"Baunilha adicionado ao estoque!"
+
+        # Chamada
+        result = ice_cream_stand.add_flavor("Baunilha")
+
+        # Validação
+        assert expected_result == result
--- test/models/test_restaurant.py
+++ test/models/test_restaurant.py
@@ -1,16 +1,72 @@
-class TestRestaurant:
+import unittest
+from restaurant import Restaurant
+
+class TestRestaurant(unittest.TestCase):
 
     def test_describe_restaurant(self):
-        assert False
+        # Setup
+        restaurant_name = "Tio Beto Restaurante"
+        cuisine_type = "Comida Brasileira"
+        restaurant = Restaurant(restaurant_name, cuisine_type)
+        expected_result = f"Esse restaurante se chama {restaurant_name} e serve {cuisine_type}.\nEsse restaurante serviu 0 consumidores desde que abriu."
+
+        # Chamada
+        result = restaurant.describe_restaurant()
+
+        # Validação
+        assert expected_result == result
 
     def test_open_restaurant(self):
-        assert False
+        # Setup
+        restaurant_name = "Tio Beto Restaurante"
+        cuisine_type = "Comida Brasileira"
+        restaurant = Restaurant(restaurant_name, cuisine_type)
+        expected_result = "Tio Beto Restaurante agora está aberto!"
+
+        # Chamada
+        result = restaurant.open_restaurant(True)
+
+        # Validação
+        assert expected_result == result
 
     def test_close_restaurant(self):
-        assert False
+        # Setup
+        restaurant_name = "Tio Beto Restaurante"
+        cuisine_type = "Comida Brasileira"
+        restaurant = Restaurant(restaurant_name, cuisine_type)
+        expected_result = "Tio Beto Restaurante agora está fechado!"
+        # Chamada
+        result = restaurant.close_restaurant(False)
+
+        # Validação
+        assert expected_result == result
 
     def test_set_number_served(self):
-        assert False
+        # Setup
+        restaurant_name = "Tio Beto Restaurante"
+        cuisine_type = "Comida Brasileira"
+        restaurant = Restaurant(restaurant_name, cuisine_type)
+        number_served = 10
+
+        # Chamada
+        result = restaurant.set_number_served(10)
+
+        # Validação
+        assert number_served == result
 
     def test_increment_number_served(self):
-        assert False
+        # Setup
+        restaurant_name = "Tio Beto Restaurante"
+        cuisine_type = "Comida Brasileira"
+        restaurant = Restaurant(restaurant_name, cuisine_type)
+        initial_customers = 5
+        increment_customers = 3
+        expected_customers = initial_customers + increment_customers
+
+        # Chamada
+        restaurant.number_served = initial_customers
+        restaurant.increment_number_served(increment_customers)
+
+        # Validação
+        assert restaurant.number_served == expected_customers
+
